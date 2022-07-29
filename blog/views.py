03079346d1bbdb9a968da8a84464364ffe4aa35e
RaoulRV <raoulradulescu@gmail.com>
2022-07-29 19:46:24 +0000

    Troubleshooting indentation error in views py

--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,19 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 class PostDetail(View):
+
     def get(self, request, slug, *args, **kwargs):
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
-    liked = False
-    if post.likes.filter(id=self.request.user.id).exists():
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-     return render(
+        return render(
             request,
             "post_detail.html",
             {
                 "post": post,
                 "liked": liked
             },
-     )
+        )
