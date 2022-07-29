e646b95a474dd1af544fbfcccdc375af41e6ae3b
RaoulRV <raoulradulescu@gmail.com>
2022-07-29 10:08:56 +0000

    Add base, index and post_detail

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
-    summernote_fields = ('content')
+
+    list_display = ('item', 'slug', 'status', 'created_on')
+    search_fields = ('item', 'description')
+    prepopulated_fields = {'slug': ('item',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('description',)
--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
         ordering = ['-created_on']
 
     def __str__(self):
-        return self.title
+        return self.item
 
     def number_of_likes(self):
             return self.likes.count()
